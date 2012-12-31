63dd548b2bc2819efeda5ff3cea4b65135d0806c
kellabyte <kell.sommers@gmail.com>
2012-12-31 13:52:12 -0500

    Upgraded IKVM to latest 7.2

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo on
-set ikvmc="..\..\lib\ikvm\ikvmc.exe"
-set ikvm="..\..\lib\ikvm"
+set ikvmc="..\..\lib\ikvm\bin\ikvmc.exe"
+set ikvm="..\..\lib\bin\ikvm"
 cd lib\solr
 %ikvmc% -out:..\solr.dll -target:library *.jar
---
+++ src/SolrIKVM/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Configuration.Install;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace SolrIKVM
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                if (args.Length == 0)
+                {
+                    RunAsConsoleApplication();
+                }
+                else if (args[0].StartsWith("/i"))
+                {
+                    InstallAsWindowsService();
+                }
+                else if (args[0].StartsWith("/u"))
+                {
+                    UninstallAsWindowsService();
+                }
+            }
+            else
+            {
+                RunAsWindowsService();
+            }
+            return 0;
+        }
+
+        private static void RunAsWindowsService()
+        {
+            ServiceBase.Run(new SolrWindowsService());
+        }
+
+        private static void UninstallAsWindowsService()
+        {
+
+            ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+        }
+
+        private static void InstallAsWindowsService()
+        {
+            ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+        }
+
+        private static void RunAsConsoleApplication()
+        {
+            try
+            {
+                var solrService = new SolrWindowsService();
+                solrService.Start();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Exception!!!!");
+                Console.WriteLine("-----------------------------");
+                WriteException(ex);
+                Console.WriteLine("-----------------------------");
+                Console.ReadKey();
+            }
+        }
+
+        public static void WriteException(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            if (ex.InnerException != null)
+            {
+                Console.WriteLine("-----------------------------");
+                WriteException(ex.InnerException);
+            }
+        }
+    }
+}
